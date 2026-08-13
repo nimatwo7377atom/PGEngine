054f9057e49772a13c3ba82b14a73724cf505bb6
nimatwo7377atom <nimatwo7373@atomicmail.io>
2026-08-13 13:56:46 +0330

    Update __init__.py

--- ppocr/modeling/heads/__init__.py
+++ ppocr/modeling/heads/__init__.py
@@ -25,6 +25,7 @@ def build_head(config):
     from .e2e_pg_head import PGHead
     from .det_ct_head import CT_Head
     from .det_sast_head_aux import SASTHeadWithAux
+    from .det_db_head_aux import DBHeadWithAux
     
     # rec head
     from .rec_ctc_head import CTCHead
